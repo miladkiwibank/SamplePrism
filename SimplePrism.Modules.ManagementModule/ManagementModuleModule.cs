1ef69837f7c8a7deb9b4acfa60d5d7272e800c7e
Chemmy <chengming0916@gmail.com>
2019-09-12 17:12:08 +0800

    新建项目结构

---
+++ SimplePrism.Modules.ManagementModule/ManagementModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using SimplePrism.Modules.ManagementModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SimplePrism.Modules.ManagementModule
+{
+    public class ManagementModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ SimplePrism.Presentation.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrism.Presentation.Services
+{
+    public class Class1
+    {
+    }
+}
