0dfa7c60e39333ab9e320a7521d71572bd76decf
Chemmy <chengming0916@gmail.com>
2020-07-16 17:39:52 +0800

    引用EF

--- SimplePrism.Persistance/DataContext.cs
+++ SimplePrism.Persistance/DataContext.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimplePrism.Persistance
 {
-    public class Class1
+    public class DataContext : DbContext
     {
+        public DataContext() : base("Name=DefaultConnecton")
+        {
+
+        }
     }
 }
