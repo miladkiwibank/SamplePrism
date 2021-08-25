fa26053170718bf37b7e63844ddbf4f7a97e9a70
Chemmy <chengming0916@outlook.com>
2021-08-25 13:44:23 +0800

    添加全局事件通知

---
+++ SimplePrism.Presentation.Services/Common/EventTopicNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrism.Presentation.Services.Common
+{
+    public static class EventTopicNames
+    {
+        public const string ShellInitlized = "Shell Initialized";
+        public static object NavigationCommandAdded;
+    }
+}
